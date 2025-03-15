d969d375a6167e0ecba3c929eaaee2c6e9c06586
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-16 02:05:09 +0300

    Courses ve CourseCategories Controller

---
+++ OnlineEdu.API/Controllers/CoursesController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.CourseDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController(IGenericService<Course> _courseServices, IMapper _mapper) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _courseServices.TGetList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            var value = _courseServices.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _courseServices.TDelete(id);
+            return Ok("Kurs Alanı Silindi");
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateCourseDto createCourseDto)
+        {
+            var newValue = _mapper.Map<Course>(createCourseDto);
+            _courseServices.TCreate(newValue);
+            return Ok("Kurs Alanı Oluşturuldu");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateCourseDto updateCourseDto)
+        {
+            var value = _mapper.Map<Course>(updateCourseDto);
+            _courseServices.TUpdate(value);
+            return Ok("Kurs Alanı Güncellendi");
+
+        }
+    }
+}
---
+++ OnlineEdu.DTO/DTOs/CourseDtos/ResultCourseDto.cs
@@ -0,0 +1,23 @@
+﻿using OnlineEdu.DTO.DTOs.CourseCategoryDtos;
+using OnlineEdu.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.DTO.DTOs.CourseDtos
+{
+    public class ResultCourseDto
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public ResultCourseCategoryDto Category { get; set; }
+        public decimal Price { get; set; }
+        public bool IsShown
+        {
+            get; set;
+        }
+}

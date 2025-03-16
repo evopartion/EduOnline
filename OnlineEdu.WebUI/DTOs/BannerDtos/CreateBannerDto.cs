1c3a39808c9750c22e0b0601e5460bf3d19eb9a7
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-17 01:22:20 +0300

    UI DTOs

---
+++ OnlineEdu.WebUI/DTOs/BannerDtos/CreateBannerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.WebUI.DTOs.BannerDtos
+{
+    public class CreateBannerDto
+    {
+        public int BannerId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl
+        {
+            get; set;
+        }
+    }
+}

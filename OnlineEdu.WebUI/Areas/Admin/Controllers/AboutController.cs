e999b86373ac65dd4bc0acbb9cbd67ceeb417245
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-17 02:13:04 +0300

    HttpClient Oluşturma

--- OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.AboutDtos;
+using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("[area]/[controller]/[action]/{id?}")]
-    public class AboutController(HttpClient _client) : Controller
+    public class AboutController : Controller
     {
+       private readonly HttpClient _client=HttpClientInstance.CreateClient();
         public IActionResult Index()
         {
+            var values = _client.GetFromJsonAsync<List<ResultAboutDto>>("abouts");
             return View();
         }
     }
---
+++ OnlineEdu.WebUI/Helpers/HttpClientInstance.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineEdu.WebUI.Helpers
+{
+    public class HttpClientInstance
+    {
+        public static HttpClient CreateClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7291/api");
+            return client;
+        }
+    }
+}

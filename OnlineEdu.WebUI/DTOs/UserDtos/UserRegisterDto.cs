2e4a8af02e389efc94212b8159a3868dd862daea
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-19 02:40:49 +0300

    Register Teması Dahil Edildi

--- OnlineEdu.WebUI/DTOs/UserDtos/UserRegisterDto.cs
+++ OnlineEdu.WebUI/DTOs/UserDtos/UserRegisterDto.cs
@@ -9,6 +9,7 @@ namespace OnlineEdu.WebUI.DTOs.UserDtos
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor")]
         public string ConfirmPassword
         {
             get; set;

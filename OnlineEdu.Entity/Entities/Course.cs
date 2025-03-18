52f4a1a8fcaa1cb1623d8d5d0e385b6ddf0be634
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-19 02:06:01 +0300

    Kullanıcı ve Kurslar Arasında İlişki

--- OnlineEdu.Entity/Entities/Course.cs
+++ OnlineEdu.Entity/Entities/Course.cs
@@ -15,6 +15,9 @@ namespace OnlineEdu.Entity.Entities
         public CourseCategory Category { get; set; }
         public decimal Price { get; set; }
         public bool IsShown { get; set; }
+        public int? AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public List<CourseRegister> CourseRegisters { get; set; }
 
     }
 }

9f701a9f4ab579bd96f1745a3818451a9cb7d362
evopartion <61056779+evopartion@users.noreply.github.com>
2025-03-23 02:01:09 +0300

    Blog Sayfası Başlangıç

--- OnlineEdu.DataAccess/Concrete/BlogRepository.cs
+++ OnlineEdu.DataAccess/Concrete/BlogRepository.cs
@@ -21,7 +21,7 @@ namespace OnlineEdu.DataAccess.Concrete
 
         public List<Blog> GetBlogsWithCategories()
         {
-            return _educontext.Blogs.Include(x => x.BlogCategory).ToList();
+            return _educontext.Blogs.Include(x => x.BlogCategory).Include(x=>x.Writer).ToList();
         }
 
         public List<Blog> GetBlogsWithCategoriesByWriterId(int id)
